f5634c41373200b8ffaceb5df2e5939bacb62c4d
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-02-17 19:55:22 -0600

    updates

--- Spring2020-master/Spring2020-master/Day4Done/game.js
+++ Spring2020-master/Spring2020-master/Day4Done/game.js
@@ -2,6 +2,7 @@ import Scene from "./Scene.js";
 import GameObject from "./GameObject.js"
 import RectangleComponent from "./RectangleComponent.js"
 import AxeBladeComponent from "./AxeBladeComponent.js"
+import CircleComponent from "./CircleComponent.js"
 import RectangleBehavior from "./RectangleBehavior.js";
 import RectangleBehavior2 from "./RectangleBehavior2.js";
 import stationaryBehavior from "./stationaryBehavior.js";
@@ -10,31 +11,30 @@ import RectComp2 from "./RectComp2.js";
 import RectanlgeBehavior from "./RectangleBehavior.js";
 
 let startScene = new Scene();
-let spinning = new GameObject(200,200);
 let tracker = new GameObject(50 ,50);
 let trackerBar = new GameObject(75,75);
 let AxeHandle = new GameObject(220,220)
+let log = new GameObject(350,350)
 
-startScene.gameObjects.push(spinning);
 startScene.gameObjects.push(tracker);
 startScene.gameObjects.push(trackerBar);
 startScene.gameObjects.push(AxeHandle);
-let spinTangle  = new RectangleComponent(100,100,"red","blue");
+startScene.gameObjects.push(log);
 let axeHandle = new RectangleComponent(175,10,"brown","black");
 let axeBlade = new AxeBladeComponent(105, 3, 50, 0,  Math.PI, "gray");
-let rectangleBehavior = new RectangleBehavior();
 let AHBehavior = new AxeHandleBehavior();
-spinning.addComponent(spinTangle);
-spinning.addComponent(rectangleBehavior);
+let logTop = new CircleComponent(55,"brown", "black")
+let logBottom = new RectangleComponent(110,160,"brown","black")
 
 AxeHandle.addComponent(axeBlade)
 AxeHandle.addComponent(axeHandle)
 AxeHandle.addComponent(AHBehavior)
-let stationaryTangle = new RectangleComponent(150, 15, "green", "black")
+log.addComponent(logBottom)
+log.addComponent(logTop)
+let measureBar = new RectComp2(200,15, "red","black")
 let trackerTangle = new RectComp2(35,35,"orange","purple", 0, 50, 50);
 let rectangleBehavior2 = new RectangleBehavior2();
-trackerBar.addComponent(stationaryTangle)
-trackerBar.addComponent(stationaryBehavior)
+trackerBar.addComponent(measureBar)
 tracker.addComponent(trackerBar);
 tracker.addComponent(trackerTangle);
 tracker.addComponent(rectangleBehavior2);
@@ -63,4 +63,3 @@ function draw(ctx) {
 }
 
 main();
-
