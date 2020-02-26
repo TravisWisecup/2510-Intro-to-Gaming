1f385c0990497e88b4444cf4b845e458d8bbb81e
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-02-26 09:27:42 -0600

    Add files via upload

--- Spring2020-master/Spring2020-master/Day4Done/game/behaviors/AxeHandleBehavior.js
+++ Spring2020-master/Spring2020-master/Day4Done/game/behaviors/AxeHandleBehavior.js
@@ -1,8 +1,13 @@
 import Base from "../../engine/Base.js"
+import Input from "../../engine/base/Input.js";
+
 
 export default class AxeHandleBehavior extends Base.Behavior{
-    AxeSwingLimit = -3.2;
+    BackSwingLimit = -3.2;
+    ForwardSwingLimit = .5;
     ForwardSwing = false;
+    speed = .05;
+    multiplier = 1;
     
     start(){
         
@@ -16,19 +21,45 @@ export default class AxeHandleBehavior extends Base.Behavior{
         this.gameObject.rotation += .3
     }
     update(){
-        if(this.ForwardSwing){
-            this.swingForward();
+
+        if (this.gameObject.rotation >= this.BackSwingLimit) {
+            this.left = true;
+            if(Input.keys['ArrowLeft'])
+            {
+                this.gameObject.rotation -= (this.speed * this.multiplier);
+                if(this.gameObject.rotation < this.BackSwingLimit){
+                    this.gameObject.rotation = this.BackSwingLimit;
+                }
+            }
+            else if(Input.keys['ArrowRight'] && this.gameObject.rotation <= this.ForwardSwingLimit){
+                this.gameObject.rotation += (this.speed * this.multiplier);
+            }
         }
-        else{
-            this.swingBack();
-        }
+        // else if(this.gameObject.rotation <= this.BackSwingLimit){
+        //     this.right = true;
+        //     if(Input.keys['ArrowRight'])
+        //     {
+        //         this.gameObject.rotation += (this.speed * this.multiplier);
+        //     }
+        //     else if(this.gameObject.rotation >= this.BackSwingLimit){
+        //         this.fill = "yellow";
+        //     }
+            
+        // }
+
+        // if(this.ForwardSwing){
+        //     this.swingForward();
+        // }
+        // else{
+        //     this.swingBack();
+        // }
      
-        if(this.gameObject.rotation >= this.AxeSwingLimit + 4){
-            this.ForwardSwing = false;
-        }
-        if(this.gameObject.rotation <= this.AxeSwingLimit){
-            this.ForwardSwing = true;
-        }
+        // if(this.gameObject.rotation >= this.AxeSwingLimit + 4){
+        //     this.ForwardSwing = false;
+        // }
+        // if(this.gameObject.rotation <= this.AxeSwingLimit){
+        //     this.ForwardSwing = true;
+        // }
 
         //this.gameObject.x = this.position;
     }
