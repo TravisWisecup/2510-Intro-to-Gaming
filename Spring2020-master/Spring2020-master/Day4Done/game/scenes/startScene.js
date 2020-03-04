52cba1831f1bcfc78b8363b662c8140663be4450
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-03-04 13:25:37 -0600

    redoing looping

--- Spring2020-master/Spring2020-master/Day4Done/game/scenes/startScene.js
+++ Spring2020-master/Spring2020-master/Day4Done/game/scenes/startScene.js
@@ -27,42 +27,51 @@ export default class startScene extends Base.Scene {
         let sun = new Sun()
         //let moon = new GameObject(140, 75)
         let background = new BackGroundDay(350, 400)
-        let treetrunk = new TreeTrunk(525,375)
-        let treebranch1 = new Branch("right", -35, 20);
-        let treebranch2 = new Branch("left", 35, 20);
-        let treebranch3 = new Branch("right", -35, -40);
-        let treebranch4 = new Branch("left", 35, -40);
-        let treebranch5 = new Branch("right", -35, -100);
-        let treebranch6 = new Branch("left", 35, -100);
-        let treebranch7 = new Branch("right", -35, -160);
-        let treebranch8 = new Branch("left", 35, -160);
-        let treebranch9 = new Branch("right", -35, -220);
-        let treebranch10 = new Branch("left", 35, -220);
-        let treebranch11 = new Branch("right", -35, -280);
-        let treebranch12 = new Branch("left", 35, -280);
+        let treetrunk;
+        let treeBranch;
+        let i;
+        let j;
+
+
         
         this.children.push(testtext);
         
         this.children.push(background)
+
+        for(i = 0; i < 10; i++)	
+        {	
+            let treeScale = randomInteger(0.4, 1.1);	
+            if(i % 2 == 0)	
+            {	
+                treetrunk = new TreeTrunk(525 + (i*15),375)	
+            }	
+            else{	
+                treetrunk = new TreeTrunk(525 - (i*15),375)	
+            }	
+            treetrunk.scaleX = treeScale;	
+            treetrunk.scaleY = treeScale;	
+            this.children.push(treetrunk);	
+            for(j = 0; j < 12; j++)	
+            {	
+                if(j % 2 == 0)	
+                    treeBranch = new Branch("left", 35, 20 - ((j/2) * 60))	
+                else{	
+                    treeBranch = new Branch("right", -35, 20 - ((j - 1)/2 * 60))	
+                }	
+                treetrunk.children.push(treeBranch);	
+            }	
+        }
+
         this.children.push(trackerBar);
         trackerBar.children.push(inputBar)
         this.children.push(axe)
         axe.children.push(axeBlade)
         this.children.push(log)
-        this.children.push(treetrunk)
-        treetrunk.children.push(treebranch1)
-        treetrunk.children.push(treebranch2)
-        treetrunk.children.push(treebranch3)
-        treetrunk.children.push(treebranch4)
-        treetrunk.children.push(treebranch5)
-        treetrunk.children.push(treebranch6)
-        treetrunk.children.push(treebranch7)
-        treetrunk.children.push(treebranch8)
-        treetrunk.children.push(treebranch9)
-        treetrunk.children.push(treebranch10)
-        treetrunk.children.push(treebranch11)
-        treetrunk.children.push(treebranch12)
+
         background.children.push(sun)
 
+        function randomInteger(min, max) {
+            return (Math.random() * (min - max) + max)
+        }
     }
 }
