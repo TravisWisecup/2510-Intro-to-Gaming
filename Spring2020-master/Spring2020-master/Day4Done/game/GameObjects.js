8ae432b90ae6d2d9502b6d27ee2cf2e8f0ec9c52
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-03-10 18:57:20 -0500

    update for xml

--- Spring2020-master/Spring2020-master/Day4Done/game/GameObjects.js
+++ Spring2020-master/Spring2020-master/Day4Done/game/GameObjects.js
@@ -1,10 +1,36 @@
 import OscillatingCircle from "./gameObjects/OscillatingCircle.js"
 import RotatingSquare from "./gameObjects/RotatingSquare.js"
-import TestText from "./gameObjects/TestText.js"
+import Axe from "./gameObjects/Axe.js"
+import AxeBlade from "./gameObjects/AxeBlade.js"
+import BackGroundDay from "./gameObjects/BackGroundDay.js"
+import Branch from "./gameObjects/Branch.js"
+import DaySky from "./gameObjects/DaySky.js"
+import Guy from "./gameObjects/Guy.js"
+import InputBar from "./gameObjects/InputBar.js"
+import Log from "./gameObjects/Log.js"
+import Moon from "./gameObjects/Moon.js"
+import OscillatingCircle from "./gameObjects/OscillatingCircle.js"
+import Sun from "./gameObjects/Sun.js"
+import TextTimer from "./gameObjects/TextTimer.js"
+import TrackerBar from "./gameObjects/TrackerBar.js"
+import TreeTrunk from "./gameObjects/TreeTrunk.js"
 
 
 export default {
   OscillatingCircle,
   RotatingSquare,
-  TestText
+  Axe,
+  AxeBlade,
+  BackGroundDay,
+  Branch,
+  DaySky,
+  Guy,
+  InputBar,
+  Log,
+  Moon,
+  OscillatingCircle,
+  Sun,
+  TextTimer,
+  TrackerBar,
+  TreeTrunk
 }
