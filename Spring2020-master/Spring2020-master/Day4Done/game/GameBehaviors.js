eeb1c65fda2bebb8b6f524c8c462f42e07fa7829
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-03-10 22:58:09 -0500

    Add files via upload

--- Spring2020-master/Spring2020-master/Day4Done/game/GameBehaviors.js
+++ Spring2020-master/Spring2020-master/Day4Done/game/GameBehaviors.js
@@ -1,15 +1,9 @@
 import CircleBehavior from "./behaviors/CircleBehavior.js";
-import RectangleBehavior from "./behaviors/RectangleBehavior.js";
 import TextBehavior from "./behaviors/TextBehavior.js";
 import AxeHandleBehavior from "./behaviors/AxeHandleBehavior.js";
 import BranchBehavior from "./behaviors/BranchBehavior.js";
-import CircleBehavior from "./behaviors/CircleBehavior.js";
 import InputBarBehavior from "./behaviors/InputBarBehavior.js";
 import OrbitBehavior from "./behaviors/OrbitBehavior.js";
-import RectangleBehavior from "./behaviors/RectangleBehavior.js";
-import RectangleBehavior2 from "./behaviors/RectangleBehavior2.js";
-import stationaryBehavior from "./behaviors/stationaryBehavior.js";
-import TextBehavior from "./behaviors/TextBehavior.js";
 import CollisionCircleBehavior from "./behaviors/CollisionCircleBehavior.js";
 import DotBehavior from "./behaviors/DotBehavior.js";
 import TreeBuilder from "./behaviors/TreeBuilder.js";
@@ -17,16 +11,11 @@ import UpdateBranchBehavior from "./behaviors/UpdateBranchBehavior.js";
 
 export default{
   CircleBehavior,
-  RectangleBehavior,
   TextBehavior,
   BranchBehavior,
   AxeHandleBehavior,
-  CircleBehavior,
   InputBarBehavior,
   OrbitBehavior,
-  RectangleBehavior,
-  RectangleBehavior2,
-  TextBehavior,
   CollisionCircleBehavior,
   DotBehavior,
   TreeBuilder,
--- Spring2020-master/Spring2020-master/Day4Done/game/GameObjects.js
+++ Spring2020-master/Spring2020-master/Day4Done/game/GameObjects.js
@@ -1,24 +1,21 @@
-import OscillatingCircle from "./gameObjects/OscillatingCircle.js"
-import RotatingSquare from "./gameObjects/RotatingSquare.js"
-import Axe from "./gameObjects/Axe.js"
-import AxeBlade from "./gameObjects/AxeBlade.js"
-import BackGroundDay from "./gameObjects/BackGroundDay.js"
-import Branch from "./gameObjects/Branch.js"
-import DaySky from "./gameObjects/DaySky.js"
-import Guy from "./gameObjects/Guy.js"
-import InputBar from "./gameObjects/InputBar.js"
-import Log from "./gameObjects/Log.js"
-import Moon from "./gameObjects/Moon.js"
-import OscillatingCircle from "./gameObjects/OscillatingCircle.js"
-import Sun from "./gameObjects/Sun.js"
-import TextTimer from "./gameObjects/TextTimer.js"
-import TrackerBar from "./gameObjects/TrackerBar.js"
-import TreeTrunk from "./gameObjects/TreeTrunk.js"
+import OscillatingCircle from "./prefabs/OscillatingCircle.js"
+import Axe from "./prefabs/Axe.js"
+import AxeBlade from "./prefabs/AxeBlade.js"
+import BackGroundDay from "./prefabs/BackGroundDay.js"
+import Branch from "./prefabs/Branch.js"
+import DaySky from "./prefabs/DaySky.js"
+import Guy from "./prefabs/Guy.js"
+import InputBar from "./prefabs/InputBar.js"
+import Log from "./prefabs/Log.js"
+import Moon from "./prefabs/Moon.js"
+import Sun from "./prefabs/Sun.js"
+import TextTimer from "./prefabs/TextTimer.js"
+import TrackerBar from "./prefabs/TrackerBar.js"
+import TreeTrunk from "./prefabs/TreeTrunk.js"
 
 
 export default {
   OscillatingCircle,
-  RotatingSquare,
   Axe,
   AxeBlade,
   BackGroundDay,
@@ -28,7 +25,6 @@ export default {
   InputBar,
   Log,
   Moon,
-  OscillatingCircle,
   Sun,
   TextTimer,
   TrackerBar,
