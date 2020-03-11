5fb95ebd1628879d863f9bfd0f1d5721e091cd73
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-03-10 19:17:09 -0500

    Update Component.js

--- Spring2020-master/Spring2020-master/Day4Done/engine/base/Component.js
+++ Spring2020-master/Spring2020-master/Day4Done/engine/base/Component.js
@@ -1,4 +1,11 @@
+/**
+ * Components are attached to game objects.
+ * Examples of components are behaviors, renderers, and colliders
+ */
+
 export default class Component{
+
+    //Reference to containing GameObject
     gameObject;
     
 
