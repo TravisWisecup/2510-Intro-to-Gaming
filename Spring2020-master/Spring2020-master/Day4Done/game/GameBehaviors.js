e2beb0018da7f68b82b0b283df4d3891c9d35edd
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-03-10 19:00:10 -0500

    xml

--- Spring2020-master/Spring2020-master/Day4Done/game/GameBehaviors.js
+++ Spring2020-master/Spring2020-master/Day4Done/game/GameBehaviors.js
@@ -1,11 +1,27 @@
 import CircleBehavior from "./behaviors/CircleBehavior.js";
 import RectangleBehavior from "./behaviors/RectangleBehavior.js";
 import TextBehavior from "./behaviors/TextBehavior.js";
+import AxeHandleBehavior from "./behaviors/AxeHandleBehavior.js";
 import BranchBehavior from "./behaviors/BranchBehavior.js";
+import CircleBehavior from "./behaviors/CircleBehavior.js";
+import InputBarBehavior from "./behaviors/InputBarBehavior.js";
+import OrbitBehavior from "./behaviors/OrbitBehavior.js";
+import RectangleBehavior from "./behaviors/RectangleBehavior.js";
+import RectangleBehavior2 from "./behaviors/RectangleBehavior2.js";
+import stationaryBehavior from "./behaviors/stationaryBehavior.js";
+import TextBehavior from "./behaviors/TextBehavior.js";
 
 export default{
   CircleBehavior,
   RectangleBehavior,
   TextBehavior,
-  BranchBehavior
+  BranchBehavior,
+  AxeHandleBehavior,
+  CircleBehavior,
+  InputBarBehavior,
+  OrbitBehavior,
+  RectangleBehavior,
+  RectangleBehavior2,
+  TextBehavior
+  
 }
