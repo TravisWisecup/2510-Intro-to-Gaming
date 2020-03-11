3313c95d0c4135764dd99fbe0c6c280daed8b418
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-03-10 19:17:52 -0500

    Update Behavior.js

--- Spring2020-master/Spring2020-master/Day4Done/engine/base/Behavior.js
+++ Spring2020-master/Spring2020-master/Day4Done/engine/base/Behavior.js
@@ -1,13 +1,14 @@
 import Component from "./Component.js"
 
+/**
+ * Behaviors are game-specific class that add logic to game objects
+ */
 export default class Behavior extends Component{
 
-    start(){
+    //Just a reminder that extending classes should have a start() function
+    start(){ }
 
-    }
-
-    update(){
-        
-    }
+    //Just a reminder that extending classes should have an update() function
+    update(){ }
 
 }
