11bd7a760d6211270ed5201df93a747d9233879f
TravisWisecup <60196748+TravisWisecup@users.noreply.github.com>
2020-04-08 19:55:09 -0500

    Add files via upload

--- Spring2020-master/Spring2020-master/Day4Done/game/behaviors/TreeBuilder.js
+++ Spring2020-master/Spring2020-master/Day4Done/game/behaviors/TreeBuilder.js
@@ -1,89 +1,54 @@
 import Base from "../../engine/Base.js"
-import UpdateBranchBehavior from "./UpdateBranchBehavior.js";
 import Branch from "../prefabs/Branch.js"
 import SceneManager from "../SceneManager.js";
-import Point from "../../engine/base/Point.js";
 import GameObjects from "../GameObjects.js"
+import Point from "../../engine/base/Point.js";
+import TreeTrunk from "../prefabs/TreeTrunk.js";
 
 
-export default class NapsterBehavior extends Base.Behavior {
+export default class TreeBuilder extends Base.Behavior {
   peons = []
-  characterX = 0;
-  characterY = 0;
-  strategyCharacter = {};
-  marginX = 100;
-  marginY = 100;
-  tilesWide = 8;
-  tilesHigh = 5;
+
+  trees = 15;
+  branches = 36;
+
   
   start() {
 
-
-
-    for (let y = 0; y < this.tilesHigh; y++) {
+    for(let i = 0; i < this.trees; i++)
+    {
+      //let treeScale = randomInteger(0.4, 1.1);	
       this.peons.push([]);
-      for (let x = 0; x < this.tilesWide; x++) {
-        let _x = x * 100 + this.marginX;
-        let _y = y * 100 + this.marginY;
-        let tile = SceneManager.instantiate(Tile, new Base.Point(_x, _y), 0);
-        this.gameObject.children.push(tile);
-        this.peons[y].push(tile);
-
-        //Randomly assign water
-        let waterBehavior = tile.getComponent(TileBehavior);
-        if (Math.random() < .1 && (y != 0 && x != 0)) {
-          waterBehavior.isWater = true;
+      let _x = 525;
+      let _y = 375;
+      if(i % 2 == 0)	
+      {	
+        _x += (i*35);	
+      }	
+      else{	
+        _x -= (i*35);
+      }	
+      let treetrunk = SceneManager.instantiate(TreeTrunk, new Base.Point(_x,_y), 0)
+      this.gameObject.children.push(treetrunk)
+      this.peons[i].push(treetrunk)
+      //treetrunk.scaleX = treeScale;	
+      //treetrunk.scaleY = treeScale;	
+      let branchX = 35;
+      let branchY = 20;
+      let branchRotate = .8;
+      for(let j = 0; j < 2; j++)	
+      {	
+        if(j % 2 == 0){
+          branchY -= ((j/2) * 20);
         }
-      }
-    }
-
-    this.select(0, 0);
-
-    //Add the strategy character
-    this.strategyCharacter = SceneManager.instantiate(GameObjects.StrategyCharacter, new Point(this.marginX, this.marginY), 0);
-
-  }
-  update() {
-    let proposedX = this.characterX;
-    let proposedY = this.characterY;
-    if (Base.Input.getKeyUp('ArrowUp')) {
-      proposedY -= 1;
-    }
-    if (Base.Input.getKeyUp('ArrowDown')) {
-      proposedY += 1
-    }
-    if (Base.Input.getKeyUp('ArrowLeft')) {
-      proposedX -= 1
-    }
-    if (Base.Input.getKeyUp('ArrowRight')) {
-      proposedX += 1
-    }
-
-    if (proposedX >= 0 && proposedX < this.tilesWide && proposedY >= 0 && proposedY < this.tilesHigh) {
-      let tileBehavior = this.peons[proposedY][proposedX].getComponent(TileBehavior);
-      if (!tileBehavior.isWater) {
-        this.characterX = proposedX;
-        this.characterY = proposedY;
-        this.strategyCharacter.x = this.marginX   + this.characterX * 100;
-        this.strategyCharacter.y = this.marginY + this.characterY * 100;
-
-        this.select(this.characterX, this.characterY);
-      }
-    }
-
-
-  }
-  select(_x, _y) {
-    for (let y = 0; y < this.tilesHigh; y++) {
-      for (let x = 0; x < this.tilesWide; x++) {
-        let tile = this.peons[y][x];
-        let behavior = tile.getComponent(TileBehavior);
-        if (x != _x || y != _y)
-          behavior.hasCharacter = false;
-        else
-          behavior.hasCharacter = true;
-      }
+        else{	
+          branchX *= -1;
+          branchRotate *= -1;
+          branchY -= ((j-1)/2 * 20);
+        }
+        let branch = SceneManager.instantiate(Branch, new Base.Point(branchX, branchY), branchRotate)
+        treetrunk.children.push(branch)
+      }	
     }
   }
-
 }
